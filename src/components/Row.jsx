3c563af35169fcb9d7d872e988a44c7926c14cde
fourers <fourersxd@gmail.com>
2023-08-13 22:38:34 +1000

    Add MUI

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,15 @@
 import React, { Component, useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import DraggableItem from './DraggableItem';
+import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
 
 export default function Row({ rowIndex, name, items }) {
-    return (
+    return (<Grid container sx={{ borderColor: 'black', borderStyle: 'solid'}}>
+        <Grid xs="auto" sx={{ padding: "1rem", textAlign: "center", backgroundColor: 'primary.main'}}>
+                <Typography variant="h3">{name}</Typography>
+        </Grid>
+        <Grid xs sx={{padding: "1rem", backgroundColor: 'grey.500'}}>
         <Droppable droppableId={rowIndex}>
             {
                 (provided, snapshot) => (
@@ -24,5 +30,6 @@ export default function Row({ rowIndex, name, items }) {
                 )
             }
         </Droppable>
+        </Grid></Grid>
     )
 }
--- src/components/TierList.jsx
+++ src/components/TierList.jsx
@@ -2,6 +2,8 @@ import React, { Component, useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import rows from './rows.json';
 import Row from './Row';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
 
 export default function TierList() {
     const [rowData, setRowData] = useState(rows);
@@ -14,12 +16,22 @@ export default function TierList() {
         <DragDropContext
             onDragEnd={onDragEnd}
         >
-            <Row
-                key={`row`}
-                rowIndex={`row-index`}
-                name={rowData[0]["name"]}
-                items={rowData[0]["items"]}
-            />
+            <Box sx={{width: '100%'}}>
+
+                {
+                    rowData.rowOrder.map((rowId) => {
+                        const rowName = rowData[rowId].name;
+                        const rowItems = rowData[rowId].items;
+                        return <Row key={rowId} rowIndex={rowId} name={rowName} items={rowItems} />
+                    })
+                }
+                {/* <Row
+                    key={`row`}
+                    rowIndex={`row-index`}
+                    name={rowData[0]["name"]}
+                    items={rowData[0]["items"]}
+                /> */}
+            </Box>
         </DragDropContext>
     );
 }
