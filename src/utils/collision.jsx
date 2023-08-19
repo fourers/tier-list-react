71ad2954690a375b2eed601dbbea2e9db3fb36c6
fourers <fourersxd@gmail.com>
2023-08-19 20:47:09 +1000

    Refactor collision

--- src/utils/collision.jsx
+++ src/utils/collision.jsx
@@ -1,25 +1,17 @@
-import { closestCorners, rectIntersection } from "@dnd-kit/core";
+import { rectIntersection } from "@dnd-kit/core";
 
 export function customCollisionDetectionAlgorithm(props) {
     const rectIntersectionCollisions = rectIntersection(props);
 
-    // Collision detection algorithms return an array of collisions
-    let collisions =
-        rectIntersectionCollisions.length > 0
-            ? rectIntersectionCollisions
-            : closestCorners(props);
-
-    const filteredCollisions = collisions.filter(
+    const filteredCollisions = rectIntersectionCollisions.filter(
         (collision) => !collision.id.startsWith("row-"),
     );
-    if (filteredCollisions.length >= 2) {
-        collisions = filteredCollisions.sort((a, b) => {
-            const aRect = a.data.droppableContainer.rect.current;
-            const bRect = b.data.droppableContainer.rect.current;
-            const aMax = Math.max(aRect.left, aRect.top);
-            const bMax = Math.max(bRect.left, bRect.top);
-            return aMax < bMax ? -1 : bMax < aMax ? 1 : 0;
-        });
-    }
-    return collisions;
+    const sortedCollisions = filteredCollisions.length >= 2 ? filteredCollisions.sort((a, b) => {
+        const aRect = a.data.droppableContainer.rect.current;
+        const bRect = b.data.droppableContainer.rect.current;
+        const aMax = Math.max(aRect.left, aRect.top);
+        const bMax = Math.max(bRect.left, bRect.top);
+        return aMax < bMax ? -1 : bMax < aMax ? 1 : 0;
+    }) : rectIntersectionCollisions;
+    return sortedCollisions;
 }
