8b2e1e6b56b216912407d2f6074b74a5ad9edd03
Tony Huynh <fourersxd@gmail.com>
2023-08-19 19:26:50 +1000

    Update collision algorithm

--- src/utils/collision.jsx
+++ src/utils/collision.jsx
@@ -4,9 +4,22 @@ export function customCollisionDetectionAlgorithm(props) {
     const rectIntersectionCollisions = rectIntersection(props);
 
     // Collision detection algorithms return an array of collisions
-    const collisions =
+    let collisions =
         rectIntersectionCollisions.length > 0
             ? rectIntersectionCollisions
             : closestCorners(props);
+
+    const filteredCollisions = collisions.filter(
+        (collision) => !collision.id.startsWith("row-"),
+    );
+    if (filteredCollisions.length >= 2) {
+        collisions = filteredCollisions.sort((a, b) => {
+            const aRect = a.data.droppableContainer.rect.current;
+            const bRect = b.data.droppableContainer.rect.current;
+            const aMax = Math.max(aRect.left, aRect.top);
+            const bMax = Math.max(bRect.left, bRect.top);
+            return aMax < bMax ? -1 : bMax < aMax ? 1 : 0;
+        });
+    }
     return collisions;
 }
